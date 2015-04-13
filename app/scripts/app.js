2e13a4cc3c7575a9083bf2724acaeed317668717
Kamran Masood <kamichatzone@yahoo.com>
2015-04-13 19:17:27 +0500

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webAppApp
+ * @description
+ * # webAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
